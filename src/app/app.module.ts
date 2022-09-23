54a51fa18bcd937bffbfb062982dd3b9ec617a4d
Aditdev <adityalawate2004@gmail.com>
2022-09-23 20:18:02 +0530

    New Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UppercaseConverterComponent } from './components/uppercase-converter/uppercase-converter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UppercaseConverterComponent
   ],
   imports: [
     BrowserModule,
